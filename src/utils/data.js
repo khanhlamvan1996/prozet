a2f006e0c8bcce98950c0c0885e92284af309f87
khanhlamvan1996 <134150116+khanhlamvan1996@users.noreply.github.com>
2023-05-30 18:36:52 +0700

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,73 @@
+import home1 from "../img/i1.png"
+import home2 from "../img/f1.png"
+import home3 from "../img/c3.png"
+import home4 from "../img/fi1.png"
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Icecream",
+    desc: "Chocolate & Vanilla",
+    price: "5.25",
+    image: home1,
+  },
+  {
+    id: 2,
+    name: "Strawberries",
+    desc: "Fresh Strawberries",
+    price: "10.25",
+    image: home2,
+  },
+  {
+    id: 3,
+    name: "Chicken Kebab",
+    desc: "Mixed Kebab Plate",
+    price: "15.25",
+    image: home3,
+  },
+  {
+    id: 4,
+    name: "Fish Kebab",
+    desc: "Mixed Fish Kebab",
+    price: "5.25",
+    image: home4,
+  },
+]
+export const categories = [
+  {
+    id: 1,
+    name: "Cơm dĩa",
+    urlParamName: "chicken",
+  },
+  {
+    id: 2,
+    name: "Cơm hộp",
+    urlParamName: "beef",
+  },
+  {
+    id: 3,
+    name: "Cơm phần",
+    urlParamName: "mutton",
+  },
+
+  {
+    id: 7,
+    name: "Cơm chiên",
+    urlParamName: "deserts",
+  },
+  {
+    id: 8,
+    name: "Nước ngọt",
+    urlParamName: "drinks",
+  },
+  {
+    id: 9,
+    name: "Trái cây",
+    urlParamName: "fruits",
+  },
+  {
+    id: 10,
+    name: "Kem",
+    urlParamName: "icecreams",
+  },
+]
